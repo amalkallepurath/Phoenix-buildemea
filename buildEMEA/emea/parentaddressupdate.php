c052a77001d50a2d4e07f854467516512a3f953f
amalkallepurath <119942253+amalkallepurath@users.noreply.github.com>
2023-12-31 02:11:27 -0800

    php files

---
+++ buildEMEA/emea/parentaddressupdate.php
@@ -0,0 +1,25 @@
+<?php
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Methods:GET,POST");
+header("Access-Control-Allow-Headers:*");
+$connection=mysqli_connect("localhost","root","","hostel");
+$request=file_get_contents("php://input");
+
+$data=json_decode($request);
+
+$paddress=$data->paddress;
+$id=$data->id;
+$connection=mysqli_connect("localhost","root","","hostel");
+if(!$connection){
+    echo"not";
+}
+else{
+   $sql=mysqli_query($connection,"update student set paddress='$paddress' where id='$id'");
+   if(!$sql){
+    echo"not update";
+   }
+   else{
+    echo"updated";
+   }
+}
+?>
